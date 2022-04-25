52435a85b73a200ba9bd1b03f04add0b7c0811b7
jjh921 <apdlwl1582@naver.com>
2022-04-25 16:52:47 +0900

    자바 1일차 : 테스트 문제

---
+++ Java/src/day1/Ex5_Test.java
@@ -0,0 +1,37 @@
+package day1;
+
+public class Ex5_Test {
+
+	public static void main(String[] args) {
+		/*
+		 * 1.	과일가게에서  과일 판매량을 기록하려한다.
+		    사과 오렌지 판매량을 기록하기 위한 변수를 선언하세요
+   			판매량 : 정수 타입
+		 */
+		int apple = 0 ; 
+		int oragne = 0 ;
+		/* 2. 학생 한명의 국어, 영어, 수학 성적을 저장하려 한다.
+		 * 이 때 필요한 변수를 선언하세요.
+		 *  성적은 정수로 관리된다.(추가)
+		 */
+		
+		int Korean = 0 ;
+		int english = 0 ;
+		int math = 0 ;
+		
+		//실수로 관리된다.
+		double Korean1 = 0 ;
+		double english1 = 0 ;
+		double math1 = 0; 
+		
+		/*
+		 * 3. 대학생의 학점을 출력하려고 한다.
+		 *  이 때 학점을 저장하는 변수를 선언하세요
+		 *  학점은 실수로 관리된다(추가) 0 ~ 4.5로 관리된다
+		 *  학점은 F~A로 관리된다. A B C D F로 관리
+		 */ 
+		double Grades = 0.0 ;
+		char Grades1 = 'F';
+	}
+
+}
