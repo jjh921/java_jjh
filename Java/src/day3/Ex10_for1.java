521d4364667dc4bc87e8338ed63b064f94bbdeca
jjh921 <apdlwl1582@naver.com>
2022-04-28 16:59:46 +0900

    자바 3일차 : Hello for문

---
+++ Java/src/day3/Ex10_for1.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class Ex10_for1 {
+
+	public static void main(String[] args) {
+		/* 콘솔에 Hello를 5번 출력하세요
+		 * 반복횟수 : i 는 1부터 5까지 1씩 증가
+		 * 초기화 : i는 1부터
+		 * 조건식 : i는 5까지
+		 * 증감식 : i는 1씩 증가
+		 * 규칙성 (실행문) : Hello를 출력
+		 * */
+		int i;
+		for( i=0; i < 5 ; i++ ) {
+			System.out.println("Hello");
+			
+		}
+	}
+
+}
